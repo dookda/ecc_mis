44d6e89d1a041dc8720465e6feb0634a0017abb8
dookda <sakda.homhuan@gmail.com>
2021-01-16 22:56:23 +0700

    add aqi

--- test.sql
+++
@@ -1,5 +0,0 @@
-SELECT Sum(popn_total)
-FROM nyc_neighborhoods neighborhoods
-JOIN nyc_census_blocks blocks
-ON ST_Intersects(neighborhoods.geom, blocks.geom)
-WHERE neighborhoods.name = 'West Village';
