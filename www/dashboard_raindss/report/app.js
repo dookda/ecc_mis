4aa83b83f70144551cf6bdf25ffd49f7e8965a01
dookda <sakda.homhuan@gmail.com>
2021-07-26 12:06:12 +0700

    add new raindss

--- www/dashboard_raindss/report/app.js
+++ www/dashboard_raindss/report/app.js
@@ -47,6 +47,14 @@ const gter = L.tileLayer('https://{s}.google.com/vt/lyrs=t,m&x={x}&y={y}&z={z}',
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
 });
 
+const rainanual = L.tileLayer.wms("https://eec-onep.online:8443/geoserver/eec/wms?", {
+    layers: "eec:rain_anual.tif",
+    name: "lyr",
+    format: "image/png",
+    transparent: true,
+    zIndex: 2
+});
+
 const lu = L.tileLayer.wms("https://eec-onep.online:8443/geoserver/eec/wms?", {
     layers: "eec:a__46_lu_eec_61",
     name: "lyr",
@@ -86,7 +94,7 @@ const pro = L.tileLayer.wms("https://eec-onep.online:8443/geoserver/eec/wms?", {
     name: "lyr",
     format: "image/png",
     transparent: true,
-    zIndex: 3
+    zIndex: 4
     // CQL_FILTER: 'pro_code=20 OR pro_code=21 OR pro_code=22 OR pro_code=23 OR pro_code=24 OR pro_code=25 OR pro_code=26 OR pro_code=27'
 });
 
@@ -103,6 +111,7 @@ const wbody = L.tileLayer.wms("https://eec-onep.online:8443/geoserver/eec/wms?",
     name: "lyr",
     format: "image/png",
     transparent: true,
+    opacity: 0.7,
     zIndex: 1
 });
 
@@ -117,10 +126,8 @@ const vill = L.tileLayer.wms("https://eec-onep.online:8443/geoserver/eec/wms?", 
 
 let lyrs = L.featureGroup().addTo(map)
 
-let eecUrl = "https://eec-onep.online:8443/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=";
+let eecUrl = "https://eec-onep.online:8443/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&legend_options=fontName:Kanit&LAYER=";
 // let rtiUrl = "https://rti2dss.com:8443/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=";
-
-$("#rainLegend").attr("src", eecUrl + "eec:rain_w20.tif");
 $("#luLegend").attr("src", eecUrl + "eec:a__46_lu_eec_61");
 $("#munLegend").attr("src", eecUrl + "eec:a__04_municiple");
 $("#proLegend").attr("src", eecUrl + "eec:a__01_prov_eec");
@@ -128,29 +135,13 @@ $("#ampLegend").attr("src", eecUrl + "eec:a__02_amphoe_eec");
 $("#tamLegend").attr("src", eecUrl + "eec:a__03_tambon_eec");
 $("#controlLegend").attr("src", eecUrl + "eec:a__06_pollution_control");
 $("#wbodyLegend").attr("src", eecUrl + "eec:a__14_w2_eec");
-
-// $("#aqiLegend").attr("src", "./marker/location-pin-green.svg");
 $("#meteoLegend").attr("src", "./marker-meteo/location-pin-green.svg");
-// $("#hpLegend").attr("src", "./img/hotspot.png");
 $("#gwLegend").attr("src", "./img/gw.png");
 $("#radarLegend").attr("src", "./img/radar.png");
 $("#villLegend").attr("src", eecUrl + "eec:a__05_village");
 
-for (let i = 2; i < 29; i++) {
-    $("#rain").append(
-        `<div class="form-check">
-            <input class="form-check-input" type="checkbox" value="zrain_w${i}"
-                id="zrain_w${i}">
-                <label class="form-check-label">
-                    ปริมาณน้ำฝนสัปดาห์ที่ ${i}
-                </label>
-                <br>
-            <img id="zrainW${i}Legend">
-        </div>`
-    )
+$("#rainanualLegend").attr("src", eecUrl + "eec:rain_anual.tif");
 
-    $("#zrainW" + i + "Legend").attr("src", `${eecUrl}eec:rain_w${i}.tif`);
-}
 
 function onLocationFound(e) {
     // latLng = e.latlng;
@@ -172,19 +163,47 @@ var lc = L.control.locate({
 }).addTo(map);
 
 // lc.start();
+pro.addTo(map)
 
 let lyr = {
     tam: tam,
     amp: amp,
-    pro: pro.addTo(map),
+    pro: pro,
     vill: vill,
     lu: lu,
     muni: muni,
     wbody: wbody,
-    pcontrol: pcontrol
+    pcontrol: pcontrol,
+    rainanual: rainanual
 }
 
-for (let i = 2; i < 29; i++) {
+const getWeekNumber = (d) => {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+    return [d.getUTCFullYear(), weekNo];
+}
+
+var yweek = getWeekNumber(new Date());
+
+for (let i = 1; i <= yweek[1]; i++) {
+    // add layer chkbox
+    let chk = i == yweek[1] ? "checked" : null;
+    $("#rain").append(
+        `<div class="form-check">
+            <input class="form-check-input" type="checkbox" value="zrain_w${i}"
+                id="zrain_w${i}" ${chk}>
+                <label class="form-check-label">
+                    ปริมาณน้ำฝนสัปดาห์ที่ ${i}
+                </label>
+                <br>
+            <img id="zrainW${i}Legend">
+        </div>`
+    )
+    // add legend
+    $("#zrainW" + i + "Legend").attr("src", `${eecUrl}eec:rain_w${i}.tif`);
+    // add layer
     lyr[`zrain_w${i}`] = L.tileLayer.wms("https://eec-onep.online:8443/geoserver/eec/wms?", {
         layers: `eec:rain_w${i}.tif`,
         name: "lyr",
@@ -192,6 +211,14 @@ for (let i = 2; i < 29; i++) {
         transparent: true,
         zIndex: 2
     })
+    i == yweek[1] ? lyr[`zrain_w${i}`].addTo(map) : null;
+}
+
+let rainLyr = 'rain_w1.tif';
+let lyrLen;
+for (let i = 2; i <= yweek[1]; i++) {
+    rainLyr += `,rain_w${i}.tif`;
+    lyrLen = i;
 }
 
 let base = {
@@ -205,14 +232,12 @@ let base = {
 // L.control.layers(baseMap, overlayMap).addTo(map);
 let refreshPage = () => {
     location.href = "./../report/index.html";
-    // console.log("ok");
 }
 
 let getDetail = (e) => {
     sessionStorage.setItem('orgid', e);
     location.href = "./../detail/index.html";
 }
-
 
 let responseWeather = axios.get(url + '/eec-api/get-weather-3hr-all');
 let loadMeteo = async () => {
@@ -512,7 +537,6 @@ $("input[type=checkbox]").change(async () => {
             initialize(apiData, optionKind);
         }
     })
-
 })
 
 $("input[name='basemap']").change(async (r) => {
@@ -526,6 +550,171 @@ $("input[name='basemap']").change(async (r) => {
     let basemap = $("input[name='basemap']:checked").val();
     base[`${basemap}`].addTo(map);
 })
+$("#hchart").html(`<div style="text-align: center;">คลิ๊กลงบนแผนที่เพื่อดูปริมาณน้ำฝนแต่ละสัปดาห์</div>`)
+let hchart = (dat) => {
+    am4core.useTheme(am4themes_animated);
+    var chart = am4core.create("hchart", am4charts.XYChart);
+    chart.paddingRight = 20;
 
+    // Add data
+    chart.data = dat;
 
+    // Create axes
+    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "week";
+    categoryAxis.renderer.minGridDistance = 50;
+    categoryAxis.renderer.grid.template.location = 0.5;
+    categoryAxis.startLocation = 0.5;
+    categoryAxis.endLocation = 0.5;
+    categoryAxis.title.text = "สัปดาห์ที่";
+    categoryAxis.title.fontSize = 12;
 
+    // Create value axis
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.baseValue = 0;
+    valueAxis.title.text = "ปริมาณน้ำฝน มม.";
+    valueAxis.title.fontSize = 12;
+
+    // Create series
+    var series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.valueY = "value";
+    series.dataFields.categoryX = "week";
+    series.strokeWidth = 2;
+    series.tensionX = 0.77;
+
+    // bullet is added because we add tooltip to a bullet for it to change color
+    var bullet = series.bullets.push(new am4charts.Bullet());
+    bullet.tooltipText = "{valueY}";
+
+    bullet.adapter.add("fill", function (fill, target) {
+        if (target.dataItem.valueY < 0) {
+            return am4core.color("#FF0000");
+        }
+        return fill;
+    })
+    var range = valueAxis.createSeriesRange(series);
+    range.value = 0;
+    range.endValue = -1000;
+    range.contents.stroke = am4core.color("#FF0000");
+    range.contents.fill = range.contents.stroke;
+
+    // // Add scrollbar
+    // var scrollbarX = new am4charts.XYChartScrollbar();
+    // scrollbarX.series.push(series);
+    // chart.scrollbarX = scrollbarX;
+
+    chart.cursor = new am4charts.XYCursor();
+}
+
+$("#weeknow").html(`สัปดาห์ปัจจุบัน: ${yweek[1]}`);
+map.on("click", async (e) => {
+    var pnt = map.latLngToContainerPoint(e.latlng);
+    var size = map.getSize()
+    var bbox = map.getBounds().toBBoxString()
+    let urlWeek = "https://eec-onep.online:8443/geoserver/wms?SERVICE=WMS" +
+        "&VERSION=1.1.1&REQUEST=GetFeatureInfo" +
+        "&QUERY_LAYERS=" + rainLyr +
+        "&LAYERS=" + rainLyr +
+        "&Feature_count=" + lyrLen +
+        "&INFO_FORMAT=application/json" +
+        "&X=" + pnt.x +
+        "&Y=" + pnt.y +
+        "&SRS=EPSG:4326" +
+        "&WIDTH=" + size.x +
+        "&HEIGHT=" + size.y +
+        "&BBOX=" + bbox;
+    await axios.get(urlWeek).then(r => {
+        let dat = [];
+        let wk = 1;
+        r.data.features.map(i => {
+            // console.log(i.properties.GRAY_INDEX);
+            dat.push({
+                "week": wk,
+                "value": i.properties.GRAY_INDEX
+            })
+            wk++;
+        })
+        hchart(dat)
+    })
+
+    let urlฺBound = "https://eec-onep.online:8443/geoserver/wms?SERVICE=WMS" +
+        "&VERSION=1.1.1&REQUEST=GetFeatureInfo" +
+        "&QUERY_LAYERS=eec:a__03_tambon_eec,eec:a__46_lu_eec_61" +
+        "&LAYERS=eec:a__03_tambon_eec,eec:a__46_lu_eec_61" +
+        "&Feature_count=3" +
+        "&INFO_FORMAT=application/json" +
+        "&X=" + pnt.x +
+        "&Y=" + pnt.y +
+        "&SRS=EPSG:4326" +
+        "&WIDTH=" + size.x +
+        "&HEIGHT=" + size.y +
+        "&BBOX=" + bbox;
+
+    await axios.get(urlฺBound).then(r => {
+        // console.log(r.data.features.length);
+        $("#latlon").html(`ตำแหน่ง lat: ${(e.latlng.lat).toFixed(2)} 
+            lon: ${(e.latlng.lng).toFixed(2)}`);
+
+        if (r.data.features.length > 0) {
+            $("#hloc").html(`${r.data.features[0].properties.tam_nam_t} 
+                ${r.data.features[0].properties.amphoe_t}
+                ${r.data.features[0].properties.prov_nam_t}`);
+            $("#landuse").html(`การใช้ประโยชน์ <span class="badge bg-success">${r.data.features[1].properties.lu_des_th}</span>`)
+        } else {
+            $("#hloc").html("");
+            $("#landuse").html("");
+        }
+    });
+
+    let urlฺAnual = "https://eec-onep.online:8443/geoserver/wms?SERVICE=WMS" +
+        "&VERSION=1.1.1&REQUEST=GetFeatureInfo" +
+        "&QUERY_LAYERS=eec:rain_anual.tif" +
+        "&LAYERS=eec:rain_anual.tif" +
+        "&Feature_count=3" +
+        "&INFO_FORMAT=application/json" +
+        "&X=" + pnt.x +
+        "&Y=" + pnt.y +
+        "&SRS=EPSG:4326" +
+        "&WIDTH=" + size.x +
+        "&HEIGHT=" + size.y +
+        "&BBOX=" + bbox;
+    // console.log(urlฺAnual);
+    await axios.get(urlฺAnual).then(r => {
+        console.log(r);
+        if (r.data.features.length > 0) {
+            console.log(r.data.features[0].properties.GRAY_INDEX);
+            // $("#anual").html(`ผลรวมน้ำฝนตั้งแต่สัปดาห์แรก: <span class="badge bg-warning "> ${(r.data.features[0].properties.GRAY_INDEX).toFixed(2)} มม. </span>`);
+            // $("#current").html(`น้ำฝนสัปดาห์นี้: <span class="badge bg-warning ">${(r.data.features[1].properties.GRAY_INDEX).toFixed(2)} มม.</span>`);
+            document.getElementById("ranual").innerHTML = `ผลรวมน้ำฝนตั้งแต่สัปดาห์แรก: <span class="badge bg-warning "> ${(r.data.features[0].properties.GRAY_INDEX).toFixed(2)} มม. </span>`
+        } else {
+            // $("#anual").html("");
+            document.getElementById("ranual").innerHTML = ""
+            // $("#current").html("");
+        }
+
+    });
+
+    let urlฺWeeknow = "https://eec-onep.online:8443/geoserver/wms?SERVICE=WMS" +
+        "&VERSION=1.1.1&REQUEST=GetFeatureInfo" +
+        "&QUERY_LAYERS=eec:rain_w" + yweek[1] + ".tif" +
+        "&LAYERS=eec:rain_w" + yweek[1] + ".tif" +
+        "&Feature_count=3" +
+        "&INFO_FORMAT=application/json" +
+        "&X=" + pnt.x +
+        "&Y=" + pnt.y +
+        "&SRS=EPSG:4326" +
+        "&WIDTH=" + size.x +
+        "&HEIGHT=" + size.y +
+        "&BBOX=" + bbox;
+    // console.log(urlฺWeeknow);
+    await axios.get(urlฺWeeknow).then(r => {
+        if (r.data.features.length > 0) {
+            document.getElementById("rcurrent").innerHTML = `น้ำฝนสัปดาห์นี้: <span class="badge bg-warning ">${(r.data.features[0].properties.GRAY_INDEX).toFixed(2)} มม.</span>`;
+        } else {
+            // $("#anual").html("");
+            // $("#rcurrent").html("");
+            document.getElementById("rcurrent").innerHTML = "";
+        }
+
+    })
+})
