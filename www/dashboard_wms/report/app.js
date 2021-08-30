9098a6617b2ccf501fd44d9ad5e4c13b8948b5fd
dookda <sakda.homhuan@gmail.com>
2021-08-30 14:13:31 +0700

    add new

--- www/dashboard_wms/report/app.js
+++ www/dashboard_wms/report/app.js
@@ -1892,7 +1892,8 @@ map.on("click", async (e) => {
 
     await axios.get(lyrInfoUrl).then(r => {
         if (r.data.features) {
-            console.log(r);
+            $("#accordion").empty();
+            console.log(r.data.features);
             r.data.features.map(i => {
                 // console.log(i.properties);
                 $("#accordion").append(`<div class="card">
@@ -1908,18 +1909,10 @@ map.on("click", async (e) => {
                 <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
                     data-parent="#accordion">
                     <div class="card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-                        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                        aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.
-                        Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente
-                        ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                        farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them
-                        accusamus labore sustainable VHS.
+                        ${i.properties.prov_nam_t}
                     </div>
                 </div>
             </div>`)
-
 
             })
             $("#infoModal").modal("show")
