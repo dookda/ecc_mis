cfa36991c591b6fc320a7b281351d348f0940c11
dookda <sakda.homhuan@gmail.com>
2021-12-16 07:46:02 +0700

    update waterlevel

--- service/waterlevel.js
+++ service/waterlevel.js
@@ -4,19 +4,32 @@ const con = require("./db");
 const eec = con.eec;
 
 app.post("/waterlevel-api/getownerdata", (req, res) => {
-    const { usrid, btype, bcode } = req.body;
+    const { usrid, bcode } = req.body;
     let sql
-    if (bcode == "ทุกจังหวัด") {
-        sql = `SELECT gid, proj_id, watername, placename, waterlevel,
-            ST_X(geom) as lon, ST_Y(geom) as lat, TO_CHAR(ndate, 'DD-MM-YYYY') as ndate, img,   
-            ST_AsGeojson(geom) as geojson  
-        FROM waterlevel WHERE usrid='${usrid}' ORDER BY ndate ASC`;
-    } else {
-        sql = `SELECT gid, proj_id, watername, placename, waterlevel,
-            ST_X(geom) as lon, ST_Y(geom) as lat, TO_CHAR(ndate, 'DD-MM-YYYY') as ndate, img,   
-            ST_AsGeojson(geom) as geojson  
-        FROM waterlevel WHERE usrid='${usrid}' AND ${btype}='${bcode}' ORDER BY ndate ASC`;
-    }
+    // if (bcode == "ทุกจังหวัด") {
+    //     sql = `SELECT gid, proj_id, watername, placename, waterlevel,
+    //         ST_X(geom) as lon, ST_Y(geom) as lat, TO_CHAR(ndate, 'DD-MM-YYYY') as ndate, img,   
+    //         ST_AsGeojson(geom) as geojson  
+    //     FROM waterlevel WHERE usrid='${usrid}' ORDER BY ndate ASC`;
+    // } else {
+    //     sql = `SELECT gid, proj_id, watername, placename, waterlevel,
+    //         ST_X(geom) as lon, ST_Y(geom) as lat, TO_CHAR(ndate, 'DD-MM-YYYY') as ndate, img,   
+    //         ST_AsGeojson(geom) as geojson  
+    //     FROM waterlevel WHERE usrid='${usrid}' AND ${btype}='${bcode}' ORDER BY ndate ASC`;
+    // }
+
+    sql = `SELECT w.gid, w.proj_id, w.watername, w.placename, w.waterlevel,
+            ST_X(w.geom) as lon, ST_Y(w.geom) as lat, 
+            TO_CHAR(w.ndate, 'DD-MM-YYYY') as ndate, img,   
+            ST_AsGeojson(w.geom) as geojson,
+            tb.prov_code, tb.amp_code, 
+            tb.tam_code, tb.prov_namt, 
+            tb.amp_namt, tb.tam_namt
+        FROM waterlevel as w
+        JOIN eec_tam_4326 as tb
+        ON ST_Contains(tb.geom, w.geom)
+        WHERE usrid='${usrid}'
+        ORDER BY ndate ASC `;
 
     eec.query(sql).then(r => {
         res.status(200).json({
--- www/form_water_lev/report_user/app.js
+++ www/form_water_lev/report_user/app.js
@@ -2,6 +2,7 @@ let urid = sessionStorage.getItem('eecid');
 let urname = sessionStorage.getItem('eecname');
 let eecauth = sessionStorage.getItem('eecauth');
 $("#usrname").text(urname);
+
 // urid ? null : location.href = "./../../form_register/login/index.html";
 urid ? null : $("#noauth").modal("show");
 
