9b021441f90e9e1f547f7acb86f84e4f19e1d713
dookda <sakda.homhuan@gmail.com>
2022-02-21 10:18:58 +0700

    update to react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import Navbar from "./Navbar";
+
+function App() {
+    return <div>
+        <Navbar></Navbar>
+    </div>
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navbar() {
+    return <div>
+        <h1>Hello</h1>
+    </div>
+}
+
+export default Navbar;
